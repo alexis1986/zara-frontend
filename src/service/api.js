7855d8727863315378dd69988711f843a78a57eb
Alexis <alexis.castellano@gmail.com>
2023-03-08 14:33:05 -0300

    add: cache de 24 horas

--- src/service/api.js
+++ src/service/api.js
@@ -1,23 +1,40 @@
+const CACHE_EXPIRATION_TIME = 24 * 60 * 60 * 1000;
+
 class ApiService {
     async getPodcasts() {
-        const response = await fetch(`https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json`);
-        const responseAsJson = await response.json();
-        return responseAsJson.feed.entry.map(podcast =>                       
-                        ({
-                            id: podcast.id.attributes["im:id"],
-                            name: podcast["im:name"].label,
-                            image: podcast["im:image"][podcast["im:image"].length - 1].label,
-                            author: podcast["im:artist"].label                        
-                        })
-                    );
+        const data = await this.useApiCall(`https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json`);
+        return data.feed.entry.map(podcast =>                       
+            ({
+                id: podcast.id.attributes["im:id"],
+                name: podcast["im:name"].label,
+                image: podcast["im:image"][podcast["im:image"].length - 1].label,
+                author: podcast["im:artist"].label                        
+            }));        
     }
 
     async getEpisodesByPodcastId(podcastId) {
         const url = `https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode`;
-        const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`);
-        const responseAsJson = await response.json();
-        return JSON.parse(responseAsJson.contents).results;
+        const data = await this.useApiCall(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`);
+        
+        return JSON.parse(data.contents).results;
     }
+
+    async useApiCall(url) {
+        let cachedData = JSON.parse(localStorage.getItem(url));
+        
+        if (cachedData && Date.now() - cachedData.timestamp < CACHE_EXPIRATION_TIME) {
+            return cachedData.data;
+        } else {
+            const response = await fetch(url);
+            const responseData = await response.json();
+            localStorage.setItem(url, JSON.stringify({
+                data: responseData,
+                timestamp: Date.now(),
+            }))
+
+            return responseData;        
+        }
+    };
 }  
 
 export default new ApiService();
