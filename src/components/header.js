f3d5bdfc958e0c35bef97eb129e6ce7b562b75f8
Alexis <alexis.castellano@gmail.com>
2023-03-08 00:27:24 -0300

    add: link en la cabecera

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header(props) {
     return (
-        <>
+        <div className="header">
+            <Link to="/">Podcasters</Link>
             {props.loading && (<p>Cargando ...</p>)}
-        </>
+        </div>
     );
 }
 
