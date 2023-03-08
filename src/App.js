8d21cdda56935a971ddadd5dd389aad14895e66b
Alexis <alexis.castellano@gmail.com>
2023-03-08 00:28:07 -0300

    update: tiempo de timeout para desaparecer el mensaje de cargando

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 100);
+    setTimeout(() => setLoading(false), 500);
   }, []);
 
   useEffect(() => {
